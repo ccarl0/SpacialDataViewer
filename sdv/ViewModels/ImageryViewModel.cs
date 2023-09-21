72082f659793b22880f115a84371f3a39d584460
ccarl0 <carlo.cecco0@gmail.com>
2023-09-21 19:58:41 +0200

    Update api key

--- sdv/ViewModels/ImageryViewModel.cs
+++ sdv/ViewModels/ImageryViewModel.cs
@@ -38,13 +38,13 @@ namespace sdv.ViewModels
                 DateTime currentDate = DateTime.Now;
 
                 // Calculate the date one month back
-                DateTime oneMonthBack = currentDate.AddMonths(-1);
+                DateTime oneMonthBack = currentDate.AddDays(-5);
 
                 // Properly formatted
                 string oneMonthBackDate = oneMonthBack.ToString("yyyy-MM-dd");
 
                 // Send GET request to the API
-                var request = new HttpRequestMessage(HttpMethod.Get, $"https://api.nasa.gov/planetary/apod?start_date={oneMonthBackDate}&api_key=0y29OS3mvF4FSFyBT04irdh5TLbAKCnlcLchKINJ");
+                var request = new HttpRequestMessage(HttpMethod.Get, $"https://api.nasa.gov/planetary/apod?start_date={oneMonthBackDate}&thumbs=True&api_key=jBR6BFhvvViOOlasSXpap8dKbpYs2nyWNY4Otum9");
 
                 //var request = new HttpRequestMessage(HttpMethod.Get, $"https://api.nasa.gov/planetary/apod?start_date=2023-08-01&api_key=0y29OS3mvF4FSFyBT04irdh5TLbAKCnlcLchKINJ");
 
